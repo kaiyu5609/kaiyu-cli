9a3c3103ed0cc2945237cf718cbc74f2b6b6f38d
kaiyu5609 <1047935422@qq.com>
2018-07-08 21:02:52 +0800

    v0.0.10

--- bin/kaiyu-surprise.js
+++ bin/kaiyu-surprise.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const path = require('path')
 const fs = require('fs')
 
 program.usage('<surprise>')
 program.command('surprise').description('a surprise')
 program.parse(process.argv)
 
-fs.readFile('./lib/love.txt', { 
+var filePath = path.join(__dirname, '../lib/love.txt')
+
+fs.readFile(filePath, {
     encoding: 'utf-8' 
 }, function(err, data) {
     if (err) {
