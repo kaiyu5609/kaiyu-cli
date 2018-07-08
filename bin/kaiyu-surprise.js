b1d7eba94c3691b97d2d8586dc71143a79d70d7e
kaiyu5609 <1047935422@qq.com>
2018-07-08 20:51:30 +0800

    v0.0.8

--- bin/kaiyu-surprise.js
+++ bin/kaiyu-surprise.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const imaging = require('imaging')
+const fs = require('fs')
 
 program.usage('<surprise>')
 program.command('surprise').description('a surprise')
 program.parse(process.argv)
 
-imaging.draw('./../lib/image/love.png', {
-    width: 90, char: '@'
-}, function(resp, status) {
-    if (status == 'success') {
-        console.log(resp)
+fs.readFile('../lib/love.txt', { 
+    encoding: 'utf-8' 
+}, function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
     }
 })
