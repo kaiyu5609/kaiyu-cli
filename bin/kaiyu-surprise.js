a4329ea6facb0a0d7edbc5bc3efc58940fa1664d
kaiyu5609 <1047935422@qq.com>
2018-07-08 20:56:13 +0800

    v0.0.9

--- bin/kaiyu-surprise.js
+++ bin/kaiyu-surprise.js
@@ -7,7 +7,7 @@ program.usage('<surprise>')
 program.command('surprise').description('a surprise')
 program.parse(process.argv)
 
-fs.readFile('../lib/love.txt', { 
+fs.readFile('./lib/love.txt', { 
     encoding: 'utf-8' 
 }, function(err, data) {
     if (err) {
